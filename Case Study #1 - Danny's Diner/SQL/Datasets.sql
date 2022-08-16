02a96dd2e30da1f7e16419d7d07e9c47cb78e4e6
John Paul Del Mundo <john.delmundo@CEGROUP.com>
2022-08-16 17:06:35 +0800

    added starter datasets

---
+++ Case Study #1 - Danny's Diner/SQL/Datasets.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE EightWeekSQLChallenge;
+GO
+
+USE EightWeekSQLChallenge;
+GO
+
+CREATE SCHEMA weekone;
+GO
+
+CREATE TABLE weekone.menu (
+	product_id INT,
+	product_name VARCHAR(5),
+	price INT
+);
+GO
+
+INSERT INTO weekone.menu (
+	product_id,
+	product_name,
+	price
+) VALUES ('1', 'sushi', '10'),
+		('2', 'curry', '15'),
+		('3', 'ramen', '12');
+GO
+
+CREATE TABLE weekone.members (
+	customer_id VARCHAR(1),
+	join_date DATETIME
+);
+GO
+
+INSERT INTO weekone.members (
+	customer_id,
+	join_date
+)
+VALUES ('A', '2021-01-07'),
+	('B', '2021-01-09');
+GO
+
+CREATE TABLE weekone.sales (
+	customer_id VARCHAR(1),
+	order_date DATETIME,
+	product_id INT
+);
+GO
+
+INSERT INTO weekone.sales (
+	customer_id, 
+	order_date, 
+	product_id
+)
+VALUES ('A', '2021-01-01', '1'),
+	('A', '2021-01-01', '2'),
+	('A', '2021-01-07', '2'),
+	('A', '2021-01-10', '3'),
+	('A', '2021-01-11', '3'),
+	('A', '2021-01-11', '3'),
+	('B', '2021-01-01', '2'),
+	('B', '2021-01-02', '2'),
+	('B', '2021-01-04', '1'),
+	('B', '2021-01-11', '1'),
+	('B', '2021-01-16', '3'),
+	('B', '2021-02-01', '3'),
+	('C', '2021-01-01', '3'),
+	('C', '2021-01-01', '3'),
+	('C', '2021-01-07', '3');
+GO
